ccf7dad405aaf3527aba32a90b203276462437c7
David Lee <Cheongmin.gitlee@gmail.com>
2025-10-15 17:11:26 -0500

    I was in a rush and forgot to commit, I completed the Hash class with no apparent compiler errors, and I implimented both constructors for the Block class

--- src/main/java/edu/grinnell/csc207/blockchain/Block.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Block.java
@@ -1,8 +1,117 @@
 package edu.grinnell.csc207.blockchain;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * A single block of a blockchain.
  */
 public class Block {
-    // TODO: fill me in!
+
+    private int num;
+    private int amount;
+    private Hash prevHash;
+    private Hash hash;
+    private long nonce;
+    /**
+     * Creates a new block from the specified parameters
+     *  Performs mining operations to discover the nonce and hash for this block
+     *  When prevHash does not exist we will hash in this order, block's number, data contained in block, 
+     *   and then nonce value.
+     *  
+     * @param num the blocks number
+     * @param amount the data(amount) transferred between two parties
+     * @param prevHash the hash value of the previous block
+     */
+    public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException{
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+
+        //Find nonce value, we will increment i until the hash values starts with 3 0's
+        long i = 0;
+        //Check if prevHash exists
+        while(true){
+            //Calculate hash value using information we have
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(ByteBuffer.allocate(4).putInt(num).array());
+            md.update(ByteBuffer.allocate(4).putInt(amount).array());
+            if(prevHash != null){
+                md.update(prevHash.getData());
+            }
+            md.update(ByteBuffer.allocate(8).putLong(i).array());
+
+            byte[] hash = md.digest();
+
+            //Check to see if has starts with atleast 3 0's
+            if(hash[0] == 0 && hash[1] == 0 && hash[2] == 0){
+                this.nonce = i;
+                this.hash = new Hash(hash);
+                break;
+            }
+            i++;
+        }
+    }
+
+    /**
+     * Creates a new block from the parameters, no need to perform mining operation
+     * @param num the blocks number 
+     * @param amount the data(amount) transferred between two parties
+     * @param prevHash the hash value of the previous block
+     * @param nonce nonce value already provided
+     */
+    public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(ByteBuffer.allocate(4).putInt(num).array());
+            md.update(ByteBuffer.allocate(4).putInt(amount).array());
+            md.update(prevHash.getData());
+            md.update(ByteBuffer.allocate(8).putLong(nonce).array());
+    }
+
+    /**
+     * Returns number of this block
+     * @return
+     */
+    public int getNum(){
+
+    }
+
+    /**
+     * Returns the amount transferred that is recorded in this block
+     * @return
+     */
+    public int getAmount(){
+
+    }
+
+    /**
+     * Returns the nonce of this block
+     * @return
+     */
+    public long getNonce(){
+
+    }
+
+    /**
+     * Returns the hash of the previous block in the blockchain
+     * @return
+     */
+    public Hash getPrevHash(){
+
+    }
+
+    /**
+     * returns the hash of this block
+     * @returns
+     */
+    public Hash getHash(){
+
+    }
+
+    /**
+     * returns a string representation of the block
+     */
+    public String toString(){
+
+    }
 }
--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,70 @@
 package edu.grinnell.csc207.blockchain;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
-    // TODO: fill me in!
+    private byte[] data;
+    /**
+     * Constructs a new Hash object that contains the given has(as an array of bites)
+     * @param data
+     */
+    public Hash(byte[] data){
+        this.data = data;
+    }
+
+    /**
+     * Returns the hash contained in this object
+     * @return
+     */
+    public byte[] getData(){
+        return data;
+    }
+
+    /**
+     * Returns true if this hash meets the criteria for validity, its first three indices contain zeroes
+     * @return
+     */
+    public boolean isValid(){
+        if(data.length > 3){
+            if(data[0] == 0 && data[1] == 0 && data[2] == 0){
+                return true;
+            } else{
+                return false;
+            }
+        } else{
+            return false;
+        }
+    }
+
+    /**
+     * Returns the string representation of the hash as a string of hexadeimal digits, 2 digits per byte
+     */
+    public String toString(){
+        StringBuilder retString = new StringBuilder();
+        for(int i =0; i<data.length; i++){
+            retString.append(String.format("%02x", Byte.toUnsignedInt(data[i])));
+        }
+        return retString.toString();
+    }
+
+    /**
+     * Returns true if this hash is structurally equal to the argument
+     */
+    public boolean equals(Object other){
+        if(other instanceof Hash){
+            //Cast other to Hash type
+            Hash otherH = (Hash) other;
+            if(Arrays.equals(this.data, otherH.data)){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
